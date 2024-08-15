e6ee819273bfcf40f2d765ad0459c95d170406e6
Paul St.Germain <paulg.stgermain@gmail.com>
2024-08-15 17:22:41 -0400

    configure auth0 provider, add .env, adjust gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain={`${process.env.AUTH0_DOMAIN}`}
+    clientId={`${process.env.AUTH0_CLIENT_ID}`}
+    authorizationParams={{
+      redirect_uri: `${window.location.origin}/app`
+    }}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
