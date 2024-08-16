fe7096648b98af415a7f09f1cd3a5572f1d7db21
Paul St.Germain <paulg.stgermain@gmail.com>
2024-08-16 17:36:48 -0400

    jobCard styling

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,11 +1,43 @@
-import { Card, CardContent, CardActions, Typography, Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Card, CardContent, CardActions, Typography, Button, FormControl, InputLabel, MenuItem, Select, CardHeader, Link } from '@mui/material';
+
+interface JobCardProps {
+  job: Job;
+}
 
 const JobCard: React.FC<JobCardProps> = ({ job }) => {
   const { id, job_title, salary, company_name, location, url, description, status } = job;
 
+  const getCardHeaderColor = (status: string) => {
+    switch (status) {
+      case 'discovered':
+        return 'primary.light';
+      case 'applied':
+        return 'success.light';
+      case 'reached out':
+        return 'info.light';
+      case 'interviewing':
+        return 'warning.light';
+      case 'offer received':
+        return 'info.dark';
+      case 'hired':
+        return 'success.main';
+      case 'ghosted':
+        return 'error.main';
+      default:
+        return 'primary.light';
+    }
+  }
+
   return (
     <div>
-      <Card sx={{ minWidth: 200, marginTop: "12px", marginBottom: "12px", textOverflow: "ellipses" }}>
+      <Card sx={{ minWidth: 200, marginTop: "12px", marginBottom: "24px", textOverflow: "ellipses" }}>
+        <CardHeader
+          sx={{ backgroundColor: getCardHeaderColor(status) }}
+          // title={`${job_title} (${salary})`}
+          // subheader={`${company_name} - ${location}`}
+          // titleTypographyProps={{ fontSize: 16, color: '#FFFFFF', fontFamily: 'Montserrat' }}
+          // subheaderTypographyProps={{ fontSize: 14, color: 'text.secondary' }}
+        />
         <CardContent>
           <Typography sx={{ fontSize: 16 }} color="text.primary" gutterBottom>
             {job_title}
@@ -22,12 +54,7 @@ const JobCard: React.FC<JobCardProps> = ({ job }) => {
           <Typography noWrap sx={{ marginTop: '8px' }}variant="body2">
             { description }
           </Typography>
-          <Typography sx={{ marginTop: '8px' }}variant="body2">
-            <a href={url} target="_blank" rel="noreferrer">
-              View Job Posting
-            </a>
-          </Typography>
-          <FormControl sx={{ mt: 1, minWidth: 120 }} size="small">
+          <FormControl sx={{ mt: 2, mb: 1, minWidth: 120 }} size="small">
             <InputLabel>Status</InputLabel>
             <Select
               value={status}
@@ -45,7 +72,18 @@ const JobCard: React.FC<JobCardProps> = ({ job }) => {
           </FormControl>
         </CardContent>
         <CardActions>
-          <Button size="small">View/Edit Job Data</Button>
+          <Button size="small" color="secondary" variant="contained">View Job Data</Button>
+          {/* <Link href={`${url}`} rel="noopener noreferrer" target="_blank"><Button size="small" color="primary" variant="outlined">To Job Post</Button></Link> */}
+          <Button size="small" color="primary" variant="outlined">
+            <a href={`https://${url}`} target="_blank" rel="noopener noreferrer">
+              To Job Post
+            </a>
+          </Button>
+          {/* <Typography sx={{ marginTop: '8px' }}variant="body2">
+            <a href={url} target="_blank" rel="noreferrer">
+              View Job Posting
+            </a>
+          </Typography> */}
         </CardActions>
       </Card>
     </div>
